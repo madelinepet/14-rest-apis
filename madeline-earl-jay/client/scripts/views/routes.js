869c6c52bb9221c403631199bfd0a12be293f8d2
MADELINE PETERS <MADELINEPET@HOTMAIL.COM>
2018-04-27 11:21:36 -0700

    We added the google books api in but it does not work.

---
+++ madeline-earl-jay/client/scripts/views/routes.js
@@ -0,0 +1,28 @@
+'use strict';
+
+page('/'
+  , (ctx, next) => app.Book.fetchAll(() => app.bookView.initIndexPage(ctx, next))
+  , (ctx, next) => app.adminView.verify(ctx, next)
+);
+page('/books/new'
+  , ctx => app.bookView.initCreateFormPage(ctx)
+);
+page('/books/search'
+  , ctx => app.bookView.initSearchFormPage()
+);
+page('/books/:book_id/update'
+  , (ctx, next) => app.Book.fetchOne(ctx, next)
+  , ctx => app.bookView.initUpdateFormPage(ctx)
+);
+page('/books/:book_id'
+  , (ctx, next) => app.Book.fetchOne(ctx, () => app.bookView.initDetailPage(ctx, next))
+  , (ctx, next) => app.adminView.verify(ctx, next)
+);
+page('/admin'
+  , () => app.adminView.initAdminPage()
+);
+
+page('/api/v1/books/find/', () => app.Book.find()
+);
+
+page();
